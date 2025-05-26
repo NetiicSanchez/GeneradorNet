387664197212d504a14f0d4e924bd6b3105c07c6
NetiicSanchez <47932946.nettic@gmail.com>
2025-05-25 22:56:21 -0600

    .

--- configuracion/db.js
+++ configuracion/db.js
@@ -1,18 +1,19 @@
     // codigo para la conexión a la base de datos
     const { Sequelize } = require('sequelize');
-    const {Sequelize} =new Sequelize (process.env.DATABASE_URL, {
+
+    const Sequelize =new Sequelize (process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
         ssl: {
-            require: true, // Esto es necesario para Heroku
-            rejectUnauthorized: false // Esto es necesario para Heroku
+            require: true, // Esto es necesario para render 
+            rejectUnauthorized: false // Esto es necesario para render
         }
     },
      logging: false, // Desactivar los logs de SQL
      timezone:'-06:00' // Ajustar la zona horaria 
     });
 
-  
+    module.exports = Sequelize; 
     // exportamos la conexión a la base de datos para poder usarla en otros archivos 
     // (sequelize es el nombre de la variable que contiene la conexión a la base de datos)
