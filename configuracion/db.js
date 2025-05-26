887f7451388d852352bfe99e28f0502bfa026151
NetiicSanchez <47932946.nettic@gmail.com>
2025-05-25 22:09:17 -0600

    modificacion el archivo db para conectarse a render

--- configuracion/db.js
+++ configuracion/db.js
@@ -1,16 +1,18 @@
     // codigo para la conexión a la base de datos
 
-    const {Sequelize} = require('sequelize');
-
-
-    const sequelize= new Sequelize('postgres', 'postgres', 'codigonet', { // nombre de la base de datos, usuario y contraseña
-        host: 'localhost',
-        dialect: 'postgres',
-        port: 5432,
-        logging: false,
-        timezone: '-06:00', // Ajusta la zona horaria según tu ubicación
-
+    const {Sequelize} =new Sequelize (process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true, // Esto es necesario para Heroku
+            rejectUnauthorized: false // Esto es necesario para Heroku
+        }
+    },
+     logging: false, // Desactivar los logs de SQL
+     timezone:'-06:00' // Ajustar la zona horaria 
     });
-    module.exports= sequelize; 
+
+  
     // exportamos la conexión a la base de datos para poder usarla en otros archivos 
     // (sequelize es el nombre de la variable que contiene la conexión a la base de datos)
